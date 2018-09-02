12f09a5c94b72c8238f6c17c78b7a2af7fcbfb4a
William Rees <reeswag@gmail.com>
2018-09-02 16:36:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+//problem 1 
+function add(a,b) {
+  return a+b 
+}
+console.log(add(4,2))
+
+//problem 2 
+function subtract(a,b) {
+  return a-b 
+}
+console.log(subtract(4,2))
+
+//problem 3 
+function multiply(a,b) {
+  return a*b 
+}
+console.log(multiply(4,2))
+
+//problem 4 
+function divide(a,b) {
+  return a/b 
+}
+console.log(divide(4,2))
+
+//problem 5 
+function inc(n) {
+  n++;
+  return n;
+}
+console.log(inc(6))
